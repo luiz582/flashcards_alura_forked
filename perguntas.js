a33369f80fc635f945d22b5e622db86221296b50
luiz582 <alves.silveira.luiz@escola.pr.gov.br>
2025-06-11 01:36:02 +0000

    luiz

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,63 @@
+
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Lingua Inglesa',
+    'Como se diz "obrigado" em inglês?',
+    'A palavra "obrigado" em inglês é "thank you".'
 )
 
+criaCartao(
+    'Química',
+    'O que é a tabela periódica?',
+    'A tabela periódica organiza os elementos químicos conforme suas propriedades e números atômicos.'
+)
+criaCartao(
+    'Física',
+    'O que é a gravidade?',
+    'A gravidade é uma força de atração entre objetos que têm massa.'
+)
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'A fotossíntese é o processo no qual as plantas transformam luz solar em energia.'
+)
+criaCartao(
+    'Artes',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci.'
+)
+criaCartao(
+    'Filosofia',
+    'Quem foi Sócrates?',
+    'Sócrates foi um filósofo grego considerado um dos fundadores da filosofia ocidental.'
+)
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Casmurro"?',
+    'O autor de "Dom Casmurro" foi Machado de Assis.'
+)
+criaCartao(
+    'Astronomia',
+    'Qual é o maior planeta do sistema solar?',
+    'O maior planeta do sistema solar é Júpiter.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável em Python?',
+    'Uma variável é um espaço na memória onde podemos armazenar valores.'
+)
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'Um número primo é aquele que só pode ser dividido por 1 e por ele mesmo.'
+)
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o maior continente do mundo?',
+    'O maior continente do mundo é a Ásia.'
 )
-
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca.'
 )
